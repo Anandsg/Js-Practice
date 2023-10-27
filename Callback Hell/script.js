29c54696ef8df66f578df4071bbf15c43525fdb8
Anandagadugin <gaduginanand984@gmail.com>
2023-10-27 20:41:02 +0530

    Call back hell

---
+++ Callback Hell/script.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+///////////////////////////////////////
+
+const renderCountry = function (data, className = '') {
+
+  const html = `
+  <article class="country ${className}">
+  <img class="country__img" src="${data.flags?.svg}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name?.common}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(+data.
+      population / 1000000).toFixed(1)}</p>
+    <p class="country__row"><span>🗣️</span>${data.languages?.hin}</p>
+    <p class="country__row"><span>💰</span>${data.currencies?.INR?.name}</p>
+  </div>
+</article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+}
+
+const getCountryAndNeighbour = function (country) {
+
+  // AJAX call for country 1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+
+  request.send();
+  request.addEventListener('load', function () {
+
+    const [data] = JSON.parse(this.responseText); // converting JSON
+    console.log(data);
+
+    // render country 1
+    renderCountry(data);
+
+    // get neighbour country 2
+    const neighbour = [data.borders?.[0]];
+
+    if (!neighbour) return;
+
+    // AJAX call for country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour}`);
+    request2.send();
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      console.log(data2);
+      const neighborCountryData = data2[0];
+      renderCountry(neighborCountryData, 'neighbour');
+    });
+  });
+};
+
+getCountryAndNeighbour('india');
+
+
+
