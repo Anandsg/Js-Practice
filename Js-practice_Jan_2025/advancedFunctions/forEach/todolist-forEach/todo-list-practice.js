08743dc8105c76f929ca006f82dccead4c90b56b
Anandagadugin <gaduginanand984@gmail.com>
2025-01-28 15:07:08 +0530

    for each

---
+++ Js-practice_Jan_2025/advancedFunctions/forEach/todolist-forEach/todo-list-practice.js
@@ -0,0 +1,61 @@
+const todoList = [];
+
+// renderTodoList();
+
+function renderTodoList() {
+  let todoListHtml = ""; // accumulator variable
+
+  todoList.forEach(function (todoObject, index) {
+    const { name, dueDate } = todoObject;
+    const html = `
+          <div>${name}</div>  
+          <div>${dueDate}</div>
+          <button onclick="
+          todoList.splice(${index}, 1)
+          renderTodoList();"
+          class="delete-btn">Delete</button>
+      `;
+    todoListHtml += html;
+  });
+  document.querySelector(".js-todo-list").innerHTML = todoListHtml;
+  //   console.log(todoListHtml);
+}
+function addTodo() {
+  const inputElement = document.querySelector(".js-name-input");
+  const name = inputElement.value;
+  //   console.log(name);
+
+  const dueDateElement = document.querySelector(".js-due-date-input");
+  const dueDate = dueDateElement.value;
+
+  //   console.log(dueDate);
+
+  if (name.trim() === "") {
+    document.querySelector(
+      ".js-name-message"
+    ).innerHTML = `Please add todo name!`;
+  } else if (dueDate.trim() === "") {
+    document.querySelector(
+      ".js-due-date-message"
+    ).innerHTML = `Please select date!`;
+  } else {
+    document.querySelector(".js-name-message").innerHTML = "";
+    document.querySelector(".js-due-date-message").innerHTML = "";
+    todoList.push({
+      name: name,
+      dueDate: dueDate,
+    });
+  }
+  console.log(todoList);
+  inputElement.value = ""; // reset input box to empty
+  dueDateElement.value = "";
+  document.querySelector(".js-todo-name");
+  renderTodoList();
+}
+
+// add todo when user hits enter
+function handleTodoKeyDown(event) {
+  if (event.key === "Enter") {
+    addTodo();
+  }
+}
