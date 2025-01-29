624f89c062db6ececc661c97dca3049d25ef2eef
Anandagadugin <gaduginanand984@gmail.com>
2025-01-29 17:52:12 +0530

    arrow function

--- Js-practice_Jan_2025/arrays/todo-list-practice.js
+++ Js-practice_Jan_2025/arrays/todo-list-practice.js
@@ -4,19 +4,18 @@ const todoList = [];
 
 function renderTodoList() {
   let todoListHtml = ""; // accumulator variable
-  for (let i = 0; i < todoList.length; i++) {
-    const todoObject = todoList[i];
+  todoList.forEach((todoObject, index) => {
     const { name, dueDate } = todoObject;
     const html = `
           <div>${name}</div>  
           <div>${dueDate}</div>
           <button onclick="
-          todoList.splice(${i}, 1)
+          todoList.splice(${index}, 1)
           renderTodoList();"
           class="delete-btn">Delete</button>
       `;
     todoListHtml += html;
-  }
+  });
   document.querySelector(".js-todo-list").innerHTML = todoListHtml;
   //   console.log(todoListHtml);
 }
